fbb24a2ace144c821644dd04b7cfde6c672397c6
Daniel DSouza <dsouza.danielbruce@in.bosch.com>
2022-03-29 23:25:07 +0530

    init

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -3,13 +3,87 @@ using Xunit;
 
 namespace TypewiseAlert.Test
 {
-  public class TypewiseAlertTest
-  {
-    [Fact]
-    public void InfersBreachAsPerLimits()
+    public class TypewiseAlertTest
     {
-      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+        [Fact]
+        public void InfersBreachAsPerLowerLimits()
+        {
+            Assert.True(TypewiseAlert.inferBreach(-1, 30) ==
+              TypewiseAlert.BreachType.TOO_LOW);
+        }
+
+        [Fact]
+        public void InfersBreachAsPerHigherLimits()
+        {
+            Assert.True(TypewiseAlert.inferBreach(31, 30) ==
+              TypewiseAlert.BreachType.TOO_HIGH);
+        }
+
+        [Fact]
+        public void InfersBreachAsPerNormalLimits()
+        {
+            Assert.True(TypewiseAlert.inferBreach(28, 30) ==
+              TypewiseAlert.BreachType.NORMAL);
+        }
+
+        [Fact]
+        public void InfersUpperLimitPassive()
+        {
+            Assert.True(TypewiseAlert.findUpperLimit(TypewiseAlert.CoolingType.PASSIVE_COOLING) ==
+              35);
+        }
+
+        [Fact]
+        public void InfersUpperLimitHi()
+        {
+            Assert.True(TypewiseAlert.findUpperLimit(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING) ==
+              45);
+        }
+
+        [Fact]
+        public void InfersUpperLimitMed()
+        {
+            Assert.True(TypewiseAlert.findUpperLimit(TypewiseAlert.CoolingType.MED_ACTIVE_COOLING) ==
+              40);
+        }
+
+        //Todo
+        [Fact]
+        public void InfersClassifyTemperatureBreach()
+        {
+            Assert.True(TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.PASSIVE_COOLING) ==
+              40);
+        }
+
+        [Fact]
+        public void InfersSendToControllerNormal()
+        {
+            Assert.True(TypewiseAlert.sendToController(TypewiseAlert.BreachType.NORMAL != true));
+        }
+
+        [Fact]
+        public void InfersSendToControllerHigh()
+        {
+            Assert.True(TypewiseAlert.sendToController(TypewiseAlert.BreachType.TOO_HIGH) != true);
+        }
+
+        [Fact]
+        public void InfersSendToControllerLow()
+        {
+            Assert.True(TypewiseAlert.sendToController(TypewiseAlert.BreachType.TOO_LOW) != true);
+        }
+
+        [Fact]
+        public void InfersSendToEmailLow()
+        {
+            Assert.True(TypewiseAlert.sendToEmail(TypewiseAlert.BreachType.TOO_LOW) != true);
+        }
+
+        [Fact]
+        public void InfersSendToEmailHigh()
+        {
+            Assert.True(TypewiseAlert.sendToEmail(TypewiseAlert.BreachType.TOO_HIGH) != true);
+        }
+
     }
-  }
 }
